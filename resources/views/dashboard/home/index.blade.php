19e4af64427661713e8587be516fe0c055feda77
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-10 10:12:29 +0430

    Add aside menu

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -14,8 +14,8 @@
 </head>
 
 <body class="min-h-screen flex flex-col text-gray-900">
-    <aside id="aside" class="hidden lg:block fixed inset-y-0 -start-64 lg:start-0 w-64 h-screen bg-gray-50 border-e border-gray-200 z-50 transition ease-in-out">
-        <div class="flex justify-between items-center h-20 px-2">
+    <aside id="aside" class="hidden lg:block fixed inset-y-0 -start-64 lg:start-0 w-64 h-screen bg-gray-50 border-e border-gray-200 z-50 transition ease-in-out overflow-y-auto">
+        <div class="flex items-center h-20 px-2">
             <h1 class="text-xl text-brand font-bold line-clamp-1">
                 <a href="/" class="block">ایمول</a>
             </h1>
@@ -26,6 +26,24 @@
                 </svg>
             </button>
         </div>
+        <ul class="px-2">
+            <li class="mb-1">
+                <a href="#" class="flex items-center py-4 px-6 rounded bg-brand text-white transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 me-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <span class="font-bold">الصفحة الرئيسية</span>
+                </a>
+            </li>
+            <li class="mb-1">
+                <a href="#" class="flex items-center py-4 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 me-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                      </svg>
+                    <span class="font-semibold">المستخدمين</span>
+                </a>
+            </li>
+        </ul>
     </aside>
 
     <main id="main" class="flex-1 lg:ms-64">
