a07be970063e95a4d383fb291c2d93c63da5794c
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-05-10 06:30:20 +0430

    Add new packages to project and config tailwind.config.js

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -4,9 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     <title>Document</title>
 </head>
 
-<body>Users</body>
+<body class="min-h-screen">Users</body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#ce2d4f',
+          '50': '#fae7eb',
+          '100': '#f5d2d9',
+          '200': '#eca8b6',
+          '300': '#e37e93',
+          '400': '#da5470',
+          '500': '#ce2d4f',
+          '600': '#a4243f',
+          '700': '#7a1b2f',
+          '800': '#50121f',
+          '900': '#27080f'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
