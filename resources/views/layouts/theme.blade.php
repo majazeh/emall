89ec70dcd0ecc5491d606c1f2ae6930cffcff275
abouhatef <itb.baravak@gmail.com>
2021-05-12 00:07:13 +0430

    clear cache

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="eMall Design System">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?v=1') }}">
 
     <title>{{ $global->title }}</title>
 </head>
 
 <body class="min-h-screen flex flex-col text-gray-900" data-page="{{ $global->page }}">
     @yield('body')
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js?v=2') }}"></script>
 </body>
 
 </html>
