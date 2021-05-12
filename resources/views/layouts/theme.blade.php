58c7305be0b28ab0b8692168d7f17a343ebf076e
abouhatef <itb.baravak@gmail.com>
2021-05-13 03:36:47 +0430

    static disable cache

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="eMall Design System">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css?v=1') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?v=' . filemtime(public_path('css/app.css'))) }}">
 
     <title>{{ $global->title }}</title>
 </head>
 
 <body class="min-h-screen flex flex-col text-gray-900" data-page="{{ $global->page }}">
     @yield('body')
-    <script src="{{ asset('js/app.js?v=2') }}"></script>
+    <script src="{{ asset('js/app.js?v=' . filemtime(public_path('js/app.js'))) }}"></script>
 </body>
 
 </html>
