f685bf29c1d1bd2ddcfbde5ff2efa66057a5e083
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-12 20:26:22 +0430

    Add fonts files and logo

--- resources/views/auth/theme.blade.php
+++ resources/views/auth/theme.blade.php
@@ -3,9 +3,9 @@
 <div class="flex-1 bg-gray-50">
     <div class="flex items-center justify-center">
         <div class="rounded w-full sm:w-80 mx-4 sm:mx-auto relative top-20">
-            <h1 class="text-center font-black text-xl text-brand mb-8">
-                <a href="/" class="direct" title="ایمول" aria-label="ایمول">ایمول</a>
-            </h1>
+            <a href="/" class="direct block mx-auto w-40 mb-4" title="ایمول" aria-label="ایمول">
+                <img src="{{ asset('images/emall.png') }}" alt="ایمول">
+            </a>
             <div data-xhr="form">
                 @yield('form')
             </div>
