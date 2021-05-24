f9ce8fecd6778be67b9d7de69bfcafdf69b13233
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-24 17:19:13 +0430

    Update search page

--- resources/views/client/products/search.blade.php
+++ resources/views/client/products/search.blade.php
@@ -2,7 +2,7 @@
 @section('main')
 <div class="p-4">
     <form class="mb-4 search">
-        <input class="border border-gray-300 h-10 px-2 rounded w-full text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand lijax" id="global_search" name="q" placeholder="البحث" data-state='true' data-lijax='300 change' data-method='get' aria-label="Search" data-xhrBase="search">
+        <input class="border border-gray-300 h-10 px-2 rounded w-full text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand lijax" id="global_search" name="q" placeholder="@lang('Search')" data-state='true' data-lijax='300 change' data-method='get' aria-label="@lang('Search')" data-xhrBase="search">
     </form>
     @include('client.products.searchFinds')
 </div>
