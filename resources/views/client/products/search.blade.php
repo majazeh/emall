ada615145c95770e1cf0925c9d9e5680595879b7
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-24 22:52:16 +0430

    Add search no result and connect to request product form

--- resources/views/client/products/search.blade.php
+++ resources/views/client/products/search.blade.php
@@ -1,9 +1,10 @@
 @extends($layouts->theme)
 @section('main')
-<div class="p-4">
+<div class="p-4 pb-16">
     <form class="mb-4 search">
         <input class="border border-gray-300 h-10 px-2 rounded w-full text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand lijax" id="global_search" name="q" placeholder="@lang('Search')" data-state='true' data-lijax='300 change' data-method='get' aria-label="@lang('Search')" data-xhrBase="search">
     </form>
     @include('client.products.searchFinds')
+    @include('client.products.searchNoResult')
 </div>
 @endsection
---
+++ resources/views/client/products/searchNoResult.blade.php
@@ -0,0 +1,3 @@
+<div class="text-gray-600 text-center mt-8">@lang('The product you are looking for could not be found')</div>
+<div class="text-gray-700 text-lg font-bold text-center mt-12">@lang('Request product')</div>
+@include('client.products.requestForm')
