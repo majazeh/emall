b6ae5714d1011ad51a46eb0788ee8ad840930b81
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-24 23:50:04 +0430

    Minor fix

--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -88,8 +88,8 @@
             <h2 class="text-lg text-brand font-bold mb-8 text-center">@lang('Contact us')</h2>
             <div class="w-full sm:w-2/3 lg:w-1/2 2xl:w-1/3 mx-auto">
                 <a href="tel:00000000000" class="flex items-center border border-brand text-brand hover:bg-brand hover:text-white transition h-12 px-4 w-full rounded dir-ltr mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
                     <span class="text-lg ms-6">96 9876 543 2222</span>
                 </a>
