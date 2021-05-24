6cee0e8e317f7683d9c40adc7d2b58a73853628a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-24 20:12:30 +0430

    Update navbar menu

--- resources/views/client/theme.blade.php
+++ resources/views/client/theme.blade.php
@@ -77,36 +77,36 @@
     <div data-xhr="main">
         @yield('main')
     </div>
-    <nav class="grid grid-cols-5 fixed inset-x-0 bottom-0 w-full h-12 rounded-t-xl shadow-lg border-t border-gray-300 bg-white">
+    <nav class="grid grid-cols-5 fixed inset-x-0 bottom-0 w-full pt-3 pb-6 sm:p-0 sm:h-12 rounded-t-xl shadow-lg border-t border-gray-300 bg-white">
         <a href="/cart" class="flex flex-col justify-center items-center text-center text-gray-600 hover:text-brand transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
-            <div class="truncate text-xs mt-1">سلتی</div>
+            <div class="truncate text-xs mt-1">@lang('My basket')</div>
         </a>
         <a href="/invoices" class="flex flex-col justify-center items-center text-center text-gray-600 hover:text-brand transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
             </svg>
-            <div class="truncate text-xs mt-1">الطلبات</div>
+            <div class="truncate text-xs mt-1">@lang('Requests')</div>
         </a>
         <a href="{{ route('products.search') }}" class="flex flex-col justify-center items-center text-center text-gray-600 hover:text-brand transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
-            <div class="truncate text-xs mt-1">البحث</div>
+            <div class="truncate text-xs mt-1">@lang('Search')</div>
         </a>
         <a href="{{ route('products.index') }}" class="flex flex-col justify-center items-center text-center text-gray-600 hover:text-brand transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
             </svg>
-            <div class="truncate text-xs mt-1">المنتجات</div>
+            <div class="truncate text-xs mt-1">@lang('Products')</div>
         </a>
         <a href="https://emall.market" class="flex flex-col justify-center items-center text-center text-gray-600 hover:text-brand transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
             </svg>
-            <div class="truncate text-xs mt-1">ایمول</div>
+            <div class="truncate text-xs mt-1">@lang('Emall')</div>
         </a>
     </nav>
 @endsection
