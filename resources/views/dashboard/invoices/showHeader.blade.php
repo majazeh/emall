786f43554f262f6ea4f413df0f4486534a9e1594
abouhatef <itb.baravak@gmail.com>
2021-05-25 22:52:21 +0430

    googlemap

--- resources/views/dashboard/invoices/showHeader.blade.php
+++ resources/views/dashboard/invoices/showHeader.blade.php
@@ -48,7 +48,7 @@
                                 </td>
                                 <td class="px-3 py-4 whitespace-nowrap">
                                     <div class="flex">
-                                        <a href="#" title="map" aria-label="map">
+                                        <a href="//google.com/maps?q={{  join(',', $invoice->location) }}" title="map" aria-label="map" target="_blank">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600 hover:text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7" />
                                             </svg>
