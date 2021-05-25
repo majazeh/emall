db6e18e1493ba2cbb2240dbe960ff260933dbf50
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-25 16:36:34 +0430

    Add banners slider

--- resources/views/client/banners.blade.php
+++ resources/views/client/banners.blade.php
@@ -1,4 +1,19 @@
-<a href="#" class="flex justify-center items-center h-24 bg-brand text-white rounded overflow-hidden">
-    {{-- <img class="w-full h-full object-cover object-center" src="" alt=""> --}}
-    <span>@lang('Banner')</span>
-</a>
+<div class="flex flex-col items-center justify-center relative">
+    <div class="w-full bg-white rounded overflow-x-hidden flex snap-x" style="height: 32vh">
+        <div class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0 bg-brand" id="slide-1">
+            <img class="w-full h-full object-cover object-center" src="" alt="">
+        </div>
+    </div>
+    <div class="w-full flex justify-between absolute p-4">
+        <a href="" class="flex justify-center items-center text-white rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+        </a>
+        <a href="" class="flex justify-center items-center text-white rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+        </a>
+    </div>
+</div>
